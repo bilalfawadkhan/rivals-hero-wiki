d0e7768b438bc17ba58630c7b602e8e1999b396d
Bilal Fawad Khan <47599282+bilalfawadkhan@users.noreply.github.com>
2025-05-19 08:13:42 +0100

    Animation added with Framer motion
    The Portrait Card animation is added to move the desired location when it is clicked on.
    
    Next Commit: Generalize the component so that different card can be used to animate/

--- app/components/Portrait.tsx
+++ app/components/Portrait.tsx
@@ -1,33 +1,68 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import {AnimatePresence, motion} from 'framer-motion';
 
-
-interface ProtraitProps {
-  src: string
-  alt?: string
-  className?: string
-  heroName?: string
-  herotype?: string
+interface PortraitProps {
+  src: string;
+  alt?: string;
+  className?: string;
+  heroName?: string;
+  herotype?: string;
 }
 
-const Portrait :React.FC<ProtraitProps> = ({src,alt ="Protrait Image",className ="",heroName = "Hero Name" , herotype='Hero Type'}) => {
+const Portrait: React.FC<PortraitProps> = ({
+  src,
+  alt = 'Portrait Image',
+  className = '',
+  heroName = 'Hero Name',
+  herotype = 'Hero Type',
+}) => {
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const handleClick = (e: React.MouseEvent,value:boolean) => {
+    e.stopPropagation();
+    value ? setIsFocused(false) : setIsFocused(true);
+    document.body.style.overflow = 'hidden'; // optional
+  };
   return (
- <Link href={'/'} target='_blank' className='inline-block'>
-     <div className='w-[200px] rounded-2xl shadow-2xl/50 flex flex-col items-center mt-12 bg-gray-700 border-gold-border border-4 overflow-hidden'>
-        <Image
-          src={src}
-          alt={alt}
-          className={` ${className}`}
-          width={200}
-          height={300}
-  
-    />
-    <h5 className='bg-gray-500 w-full text-center text-white'>{heroName}</h5>
-    <p className='bg-amber-600 w-full text-center'>{herotype}</p>
-      </div>
- </Link>
-  )
-}
+    <>
+    <AnimatePresence> 
+      {isFocused &&(
+      <motion.div
+      layoutId="portrait"
+       className={"w-[200px] rounded-2xl shadow-2xl flex flex-col items-center mt-12 bg-gray-700 border-gold-border border-4 overflow-hidden "}
+       initial={{scale: 0}}
+       animate={{scale: 2}}
+       style={{transformOrigin: 'left center',
+        top: '50%',
+        left: '8%',
+        position:'fixed',
+        transform: 'translateY(-50%)',
+       }}
+       onClick={(e) => handleClick(e, isFocused)}
+      >
+        <Image src={src} alt={alt} className={className} width={200} height={300} />
+        <h5 className="bg-gray-500 w-full text-center text-white">{heroName}</h5>
+        <p className="bg-amber-600 w-full text-center">{herotype}</p>
+      </motion.div>
+  )}
 
-export default Portrait
+  {!isFocused && (
+      <motion.div
+      layoutId="portrait"
+       className={" w-[200px] rounded-2xl shadow-2xl flex flex-col items-center mt-12 bg-gray-700 border-gold-border border-4 overflow-hidden "}
+       onClick={(e) => handleClick(e, isFocused)}
+      >
+        <Image src={src} alt={alt} className={className} width={200} height={300} />
+        <h5 className="bg-gray-500 w-full text-center text-white">{heroName}</h5>
+        <p className="bg-amber-600 w-full text-center">{herotype}</p>
+      </motion.div>
+  )}
+      </AnimatePresence>
+
+    </>
+  );
+};
+
+export default Portrait;
